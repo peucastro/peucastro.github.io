bdba39b3ebf1be9707910fcd65c3ddbbb7898b4e
Pedro Castro <falecompedroac@gmail.com>
2025-02-24 21:32:12 +0000

    configured github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://peucastro.github.io",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
