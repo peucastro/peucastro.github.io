3f782be1ec6be60b36dfb7b94422574c3b368f30
Pedro Castro <falecompedroac@gmail.com>
2025-03-02 16:37:14 +0000

    initial projects page refactor

--- src/content/config.ts
+++ src/content/config.ts
@@ -16,8 +16,27 @@ const blogSchema = z.object({
 
 export type BlogSchema = z.infer<typeof blogSchema>;
 
+const projectSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  pubDate: z.coerce.date(),
+  img: z.string().optional(),
+  url: z.string(),
+  badge: z.string().optional(),
+  tags: z
+    .array(z.string())
+    .refine((items) => new Set(items).size === items.length, {
+      message: "tags must be unique",
+    })
+    .optional(),
+});
+
+export type ProjectSchema = z.infer<typeof projectSchema>;
+
 const blogCollection = defineCollection({ schema: blogSchema });
+const projectCollection = defineCollection({ schema: projectSchema });
 
 export const collections = {
   blog: blogCollection,
+  projects: projectCollection,
 };
---
+++ src/content/projects/project-demo.md
@@ -0,0 +1,8 @@
+---
+title: "Demo Project 1"
+description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+img: "/social_img.webp"
+url: "#"
+pubDate: "Mar 01 2025"
+badge: "NEW"
+---
