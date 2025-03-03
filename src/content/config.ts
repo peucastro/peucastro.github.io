7820c36fac99dc76a3017dab76baef81d0598a4a
Pedro Castro <falecompedroac@gmail.com>
2025-03-03 15:40:01 +0000

    updated project structure

--- src/content/config.ts
+++ src/content/config.ts
@@ -21,7 +21,6 @@ const projectSchema = z.object({
   title: z.string(),
   description: z.string(),
   pubDate: z.coerce.date(),
-  updatedDate: z.coerce.date().optional(),
   heroImage: z.string().optional(),
   badge: z.string().optional(),
   tags: z
