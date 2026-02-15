546a45c3c5642d9992e989b56c9a9c90e1ce621f
Pedro Castro <falecompedroac@gmail.com>
2026-02-15 16:57:16 +0000

    deleted all files

--- src/config.ts
+++
@@ -1,4 +0,0 @@
-export const SITE_TITLE = "Pedro Castro";
-export const SITE_DESCRIPTION = "My personal website.";
-export const GENERATE_SLUG_FROM_TITLE = true;
-export const TRANSITION_API = true;
